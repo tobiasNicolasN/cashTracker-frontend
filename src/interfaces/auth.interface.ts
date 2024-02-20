44c9d0afee35ed8d50d8b60596f866f836dc8bc1
TobiasNicolasN <tobias.nicolas001@gmail.com>
2024-02-20 15:32:15 -0300

    Auth interface created

--- src/interfaces/auth.interface.ts
+++ src/interfaces/auth.interface.ts
@@ -8,3 +8,13 @@ export interface ILogin {
     email: string,
     password: string
 }
+
+export interface IUser {
+    username:  string;
+    email:     string;
+    password:  string;
+    _id:       string;
+    createdAt: Date;
+    updatedAt: Date;
+    __v:       number;
+}
