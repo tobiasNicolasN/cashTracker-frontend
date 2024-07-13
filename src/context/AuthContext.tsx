4cd32f4d0edacfa7742ce54920094853f103bb69
Tobias <tobias.nicolas001@gmail.com>
2024-07-13 02:15:48 -0300

    authControllers started
    register finished
    login finished
    logout with errors

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -7,14 +7,31 @@ interface IProps {
 interface IAuthContext {
   authenticated: boolean;
   setAuthenticated: (newState: boolean) => void;
+  user: IUser | null;
+  setUser: React.Dispatch<React.SetStateAction<IUser | null>>;
+  register: (username: string, email: string, password: string) => Promise<any>;
+  login: (email: string, password: string) => Promise<any>;
+  logout: () => Promise<void>;
 }
 
-const initialValue = {
-  authenticated: false,
-  setAuthenticated: () => {},
-};
+interface IUser {
+  id: string;
+  username: string;
+  email: string;
+  password: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
 
-export const AuthContext = createContext<IAuthContext>(initialValue);
+export const AuthContext = createContext<IAuthContext>({
+  authenticated: false,
+  user: null,
+  setUser: () => {},
+  setAuthenticated: () => {},
+  register: async () => {},
+  login: async () => {},
+  logout: async () => {},
+});
 
 export const useAuth = () => {
   const context = useContext(AuthContext);
@@ -22,13 +39,99 @@ export const useAuth = () => {
   return context;
 };
 
+const baseUrl = `${import.meta.env.VITE_BASE_URL!}/api/`;
+
 export const AuthProvider = ({ children }: IProps) => {
-  const [authenticated, setAuthenticated] = useState(
-    initialValue.authenticated
-  );
+  const [authenticated, setAuthenticated] = useState(false);
+  const [user, setUser] = useState<IUser | null>(null);
+
+  const register = async (
+    username: string,
+    email: string,
+    password: string
+  ) => {
+    try {
+      const res = await fetch(`${baseUrl}register`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username,
+          email,
+          password,
+        }),
+      });
+      const data = await res.json();
+
+      if (!res.ok) {
+        throw new Error(data.error);
+      }
+
+      setAuthenticated(true); // Autoriza al usuario
+      setUser(data);
+      console.log(data);
+      return data;
+    } catch (error) {
+      console.error("Fallo al registrar: ", error);
+    }
+  };
+
+  const login = async (email: string, password: string) => {
+    try {
+      const res = await fetch(`${baseUrl}login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email,
+          password,
+        }),
+      });
+      const data = await res.json();
+
+      if (!res.ok) {
+        throw new Error(data.error); // Si la respuesta es negativa se fuerza un error
+      }
+
+      setAuthenticated(true);
+      console.log(data);
+      return data;
+    } catch (error) {
+      console.error("Fallo al logearse: ", error);
+    }
+  };
+
+  const logout = async () => {
+    try {
+      const res = await fetch(`${baseUrl}logout`, {
+        method: "POST",
+      });
+      const data = await res.json();
+
+      if (!res.ok) {
+        throw new Error(data);
+      }
+      setAuthenticated(false);
+      console.log(data);
+    } catch (error) {
+      console.error("Fallo al deslogear: ", error);
+    }
+  };
 
   return (
-    <AuthContext.Provider value={{ authenticated, setAuthenticated }}>
+    <AuthContext.Provider
+      value={{
+        authenticated,
+        setAuthenticated,
+        register,
+        login,
+        logout,
+        user,
+        setUser,
+      }}
+    >
       {children}
     </AuthContext.Provider>
   );
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,7 +1,54 @@
+import { useAuth } from "../context/AuthContext";
+
 function LandingPage() {
+  const { register, login, logout } = useAuth();
+
+  const handleRegister = async () => {
+    try {
+      await register("prueba5", "prueba5@gmail.com", "12345678");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleLogin = async () => {
+    try {
+      await login("prueba3@gmail.com", "12345678");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleLogout = async () => {
+    try {
+      await logout();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div>LandingPage</div>
-  )
+    <div className="flex flex-col gap-3 p-2">
+      <button
+        className="text-xl bg-slate-700 w-28 rounded-lg p-2"
+        onClick={() => handleRegister()}
+      >
+        register
+      </button>
+      <button
+        className="text-xl bg-slate-700 w-28 rounded-lg p-2"
+        onClick={() => handleLogin()}
+      >
+        login
+      </button>
+      <button
+        className="text-xl bg-slate-700 w-28 rounded-lg p-2"
+        onClick={() => handleLogout()}
+      >
+        logout
+      </button>
+    </div>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
